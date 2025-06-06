04725aa9303eaa23834271546e18ebe259fecd54
Thiago <104965696+Thiagonline@users.noreply.github.com>
2025-06-06 14:24:42 -0300

    style: Corrige formatação em src/setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
   app.use(
     createProxyMiddleware(["/login", "/callback", "/logout", "/checkAuth", "graphql"], {
       // CORREÇÃO: Usar VITE_BACKEND_PORT, que é o nome correto da variável no .env
-      target: `http://localhost:${process.env.VITE_BACKEND_PORT}`, 
+      target: `http://localhost:${process.env.VITE_BACKEND_PORT}`,
       changeOrigin: true,
       logLevel: "debug",
     })
