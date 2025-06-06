593618d438af0622e7feb996aeeb438c2b282800
Thiago <104965696+Thiagonline@users.noreply.github.com>
2025-06-05 22:20:35 -0300

    style: Fix code style issues with Prettier

--- cypress/e2e/login.spec.js
+++ cypress/e2e/login.spec.js
@@ -1,52 +1,52 @@
 // cypress/e2e/login.spec.js
 
-describe('Funcionalidades de Autenticação (Login e Registro)', () => {
+describe("Funcionalidades de Autenticação (Login e Registro)", () => {
   beforeEach(() => {
-    cy.visit('/signin');
+    cy.visit("/signin");
   });
 
   // --- Testes de Login ---
 
-  it('CT01: Deve fazer login com sucesso com credenciais válidas', () => {
-    const usernameValido = 'Dina20'; // Ou 'Heath93'
-    const passwordValida = 's3cret';
+  it("CT01: Deve fazer login com sucesso com credenciais válidas", () => {
+    const usernameValido = "Dina20"; // Ou 'Heath93'
+    const passwordValida = "s3cret";
 
     cy.log(`CT01: Tentando login com ${usernameValido}/${passwordValida}`);
     cy.get('[data-test="signin-username"]').type(usernameValido);
     cy.get('[data-test="signin-password"]').type(passwordValida);
     cy.get('[data-test="signin-submit"]').click();
 
-    cy.url().should('include', '/');
-    cy.get('[data-test="sidenav-user-balance"]').should('be.visible');
+    cy.url().should("include", "/");
+    cy.get('[data-test="sidenav-user-balance"]').should("be.visible");
   });
 
-  it('CT02: Deve exibir mensagem de erro ao tentar fazer login com credenciais inválidas', () => {
-    cy.get('[data-test="signin-username"]').type('usuario.invalido@example.com');
-    cy.get('[data-test="signin-password"]').type('senhaerrada');
+  it("CT02: Deve exibir mensagem de erro ao tentar fazer login com credenciais inválidas", () => {
+    cy.get('[data-test="signin-username"]').type("usuario.invalido@example.com");
+    cy.get('[data-test="signin-password"]').type("senhaerrada");
     cy.get('[data-test="signin-submit"]').click();
 
     cy.get('[data-test="signin-error"]')
-      .should('be.visible')
-      .and('contain', 'Username or password is invalid');
-    cy.url().should('include', '/signin');
+      .should("be.visible")
+      .and("contain", "Username or password is invalid");
+    cy.url().should("include", "/signin");
   });
 
   it('CT03: Deve redirecionar para a página de registro ao clicar no link "Sign Up"', () => {
     cy.get('[data-test="signup"]').click();
-    cy.url().should('include', '/signup');
-    cy.get('[data-test="signup-title"]').should('be.visible').and('contain', 'Sign Up');
+    cy.url().should("include", "/signup");
+    cy.get('[data-test="signup-title"]').should("be.visible").and("contain", "Sign Up");
   });
 
   // --- Testes de Registro de Usuário ---
 
-  it('CT04: Deve registrar um novo usuário com sucesso com informações válidas', () => {
-    cy.visit('/signup');
+  it("CT04: Deve registrar um novo usuário com sucesso com informações válidas", () => {
+    cy.visit("/signup");
 
     const timestamp = new Date().getTime();
     const newFirstName = `Novo${timestamp}`;
     const newLastName = `Teste${timestamp}`;
     const newUsername = `usuario.${timestamp}@example.com`;
-    const newPassword = 'SenhaSegura123!';
+    const newPassword = "SenhaSegura123!";
 
     cy.get('[data-test="signup-first-name"]').type(newFirstName);
     cy.get('[data-test="signup-last-name"]').type(newLastName);
@@ -56,20 +56,20 @@ describe('Funcionalidades de Autenticação (Login e Registro)', () => {
 
     cy.get('[data-test="signup-submit"]').click();
 
-    cy.url().should('include', '/signin');
-    cy.contains('Sign In').should('be.visible');
+    cy.url().should("include", "/signin");
+    cy.contains("Sign In").should("be.visible");
   });
 
-  it('CT05: Deve manter o botão de registro desabilitado quando campos obrigatórios não forem preenchidos', () => {
-    cy.visit('/signup');
+  it("CT05: Deve manter o botão de registro desabilitado quando campos obrigatórios não forem preenchidos", () => {
+    cy.visit("/signup");
 
     // Preenche só o primeiro campo
-    cy.get('[data-test="signup-first-name"]').type('Incompleto');
+    cy.get('[data-test="signup-first-name"]').type("Incompleto");
 
     // Verifica se o botão está desabilitado
-    cy.get('[data-test="signup-submit"]').should('be.disabled');
+    cy.get('[data-test="signup-submit"]').should("be.disabled");
 
     // Garante que continua na mesma página
-    cy.url().should('include', '/signup');
+    cy.url().should("include", "/signup");
   });
 });
